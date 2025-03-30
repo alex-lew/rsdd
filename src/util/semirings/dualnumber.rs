b5cb72e197097265dfba19ebafc02d2d0a6ac1ba
mercush <barba@mit.edu>
2025-03-30 12:55:57 -0400

    dualnumber changes

--- src/util/semirings/dualnumber.rs
+++ src/util/semirings/dualnumber.rs
@@ -2,57 +2,59 @@ use super::semiring_traits::*;
 use std::{fmt::Display, ops};
 use serde::{Serialize, Deserialize};
 
-#[derive(Clone, Debug, PartialEq, Copy, Serialize, Deserialize)]
-pub struct DualNumber<const N: usize>(pub f64, pub [f64; N]);
+const NPARTIALS: usize = 3;
 
-impl<const N: usize> Display for DualNumber<N> {
+#[derive(Clone, Debug, PartialEq, Copy, Serialize, Deserialize)]
+pub struct DualNumber(pub f64, pub [f64; NPARTIALS]);
+
+impl Display for DualNumber {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {:?})", self.0, self.1)
     }
 }
 
-impl<const N: usize> ops::Add for DualNumber<N> {
+impl ops::Add for DualNumber {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let mut result_derivs = [0.0; N];
-        for i in 0..N {
+        let mut result_derivs = [0.0; NPARTIALS];
+        for i in 0..NPARTIALS {
             result_derivs[i] = self.1[i] + rhs.1[i];
         }
         Self(self.0 + rhs.0, result_derivs)
     }
 }
 
-impl<const N: usize> ops::Mul for DualNumber<N> {
+impl ops::Mul for DualNumber {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        let mut result_derivs = [0.0; N];
-        for i in 0..N {
+        let mut result_derivs = [0.0; NPARTIALS];
+        for i in 0..NPARTIALS {
             result_derivs[i] = self.0 * rhs.1[i] + self.1[i] * rhs.0;
         }
         Self(self.0 * rhs.0, result_derivs)
     }
 }
 
-impl<const N: usize> ops::Sub for DualNumber<N> {
+impl ops::Sub for DualNumber {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        let mut result_derivs = [0.0; N];
-        for i in 0..N {
+        let mut result_derivs = [0.0; NPARTIALS];
+        for i in 0..NPARTIALS {
             result_derivs[i] = self.1[i] - rhs.1[i];
         }
         Self(self.0 - rhs.0, result_derivs)
     }
 }
 
-impl<const N: usize> Semiring for DualNumber<N> {
+impl Semiring for DualNumber {
     fn one() -> Self {
-        Self(1.0, [0.0; N])
+        Self(1.0, [0.0; NPARTIALS])
     }
 
     fn zero() -> Self {
-        Self(0.0, [0.0; N])
+        Self(0.0, [0.0; NPARTIALS])
     }
 }
